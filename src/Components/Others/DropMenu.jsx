740423dabab9a641284a2f89c397f1b689b0c92d
Сергей Феоктистов <86980313+PerchHunter@users.noreply.github.com>
2022-07-21 20:47:03 +0500

    Мелкие правки

--- src/Components/Others/DropMenu.jsx
+++ src/Components/Others/DropMenu.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./css/styles.css";
+
 import { Menu, MenuItem } from "@mui/material";
 
 export default function DropMenu({ point, anchorElem, handleClose }) {
